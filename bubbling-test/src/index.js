daa504fde29f09ea53134cbfb373b6d3a91e072b
seoyoon park <yeggam12@gmail.com>
2021-04-03 00:05:25 +0900

    버블링 이슈1

--- bubbling-test/src/index.js
+++ bubbling-test/src/index.js
@@ -1,1 +1,20 @@
 // 코드를 작성해주세요!
+
+const btn1 = document.querySelector('#btn-1');
+const btn2 = document.querySelector('#btn-2');
+const btn3 = document.querySelector('#btn-3');
+const btn4 = document.querySelector('#btn-4');
+
+btn1.addEventListener('click', () => {
+    alert(btn1.innerHTML);
+})
+btn2.addEventListener('click', () => {
+    alert(btn2.innerHTML);
+})
+btn3.addEventListener('click', () => {
+    alert(btn3.innerHTML);
+})
+btn4.addEventListener('click', () => {
+    alert(btn4.innerHTML);
+})
+
