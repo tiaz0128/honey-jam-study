0eabd151c2b9b51c7b8ead4e8b851fb6585931e2
bom <kimu_u@naver.com>
2021-04-05 02:28:27 +0900

    "iss12"

--- domhandling-test/src/index.js
+++ domhandling-test/src/index.js
@@ -1,1 +1,8 @@
 // 코드를 작성해주세요!
+const changeColor = document.querySelector('#changeColor-btn');
+const tree = document.querySelector('#tree');
+
+
+changeColor.addEventListener('click', () => {
+    tree.childNodes[1].childNodes[1].childNodes[1].childNodes[1].classList.add('change');
+    })
