2d179527f732a9b5697346ecbdc90a88455d7f6a
bom <kimu_u@naver.com>
2021-04-04 23:51:57 +0900

    "iss1"

--- bubbling-test/src/index.js
+++ bubbling-test/src/index.js
@@ -1,1 +1,18 @@
 // 코드를 작성해주세요!
+const btn1 = document.querySelector('#btn-1');
+const btn2 = document.querySelector('#btn-2');
+const btn3 = document.querySelector('#btn-3');
+const btn4 = document.querySelector('#btn-4');
+
+btn1.addEventListener('click', () => {
+    alert(btn1.textContent);
+})
+btn2.addEventListener('click', () => {
+    alert(btn2.textContent);
+})
+btn3.addEventListener('click', () => {
+    alert(btn3.textContent);
+})
+btn4.addEventListener('click', () => {
+    alert(btn4.textContent);
+})
