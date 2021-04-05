2e9d72e18375bb5be6b4881270af2bab30edf075
MinJeung-Kim <focso5@knou.ac.kr>
2021-04-05 22:17:31 +0900

    Add removeChild function

--- bubbling-test/src/index.js
+++ bubbling-test/src/index.js
@@ -1,1 +1,15 @@
-// 코드를 작성해주세요!
+'user strict';
+
+const delBtn = document.querySelector('#delBtn');
+const app = document.querySelector('#app');
+
+delBtn.addEventListener('click', () => {
+  const btns = document.querySelectorAll('.btn');
+
+  btns.forEach((btn) => {
+    const even = btn.getAttribute('data-item-id');
+    if (even % 2 === 0) {
+      app.removeChild(btn);
+    }
+  });
+});
