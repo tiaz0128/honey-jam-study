c19d88209b7364f1106bf6bb71f36149436fdcde
tiaz0128 <tiaz0128.dev@gmail.com>
2021-04-06 21:31:46 +0900

    rendering-test 추가

---
+++ rendering-test/src/data.js
@@ -0,0 +1,5 @@
+export const todoList = [
+  { title: 'JS 공부하기', isComplete: false },
+  { title: '자바 스프링 공부하기', isComplete: true },
+  { title: '밥먹기', isComplete: true },
+]
---
+++ rendering-test/src/index.js
@@ -0,0 +1,9 @@
+import { todoList } from './data.js'
+
+const app = document.querySelector('#app')
+
+function render() {
+  // 여기에 코딩해주세요!
+}
+
+render()
