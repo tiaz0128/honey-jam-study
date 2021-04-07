fad49b159a1b73de86fd258378bf52adef8844f5
seoyoon park <yeggam12@gmail.com>
2021-04-08 00:10:52 +0900

    iss11

--- bubbling-test/src/index.js
+++ bubbling-test/src/index.js
@@ -1,1 +1,13 @@
 // 코드를 작성해주세요!
+
+const delBtn = document.querySelector('#delBtn');
+const btns = document.querySelectorAll('.btn');
+
+delBtn.addEventListener('click', () => {
+    btns.forEach((btn) => {
+        //console.log(btn.dataset.itemId)
+        if(btn.dataset.itemId % 2 == 0) {
+            btn.remove('btn');
+        }
+    })
+})
