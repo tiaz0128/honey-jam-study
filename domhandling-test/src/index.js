412c709fa9a5599cc2b6b7d59b53b8cc934f2689
bom <kimu_u@naver.com>
2021-04-20 23:12:23 +0900

    "querySelector 이용"

--- domhandling-test/src/index.js
+++ domhandling-test/src/index.js
@@ -1,8 +1,14 @@
 // 코드를 작성해주세요!
 const changeColor = document.querySelector('#changeColor-btn');
 const tree = document.querySelector('#tree');
+const treeLi = tree.querySelector('li');
+const Mammals = treeLi.querySelector('li');
+const cows = Mammals.querySelector('li');
+const MammalsUl = Mammals.querySelector('ul');
 
-
+console.log(Mammals.querySelector('li'))
 changeColor.addEventListener('click', () => {
-    tree.childNodes[1].childNodes[1].childNodes[1].childNodes[1].classList.add('change');
+    MammalsUl.classList.add('change');
+    cows.classList.add('change');
     })
+
