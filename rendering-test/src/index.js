f6a062e73b363a4ad11ecdf5a85eacf4405b594e
bom <kimu_u@naver.com>
2021-04-21 00:50:06 +0900

    "잘한거같음"

--- rendering-test/src/index.js
+++ rendering-test/src/index.js
@@ -4,6 +4,11 @@ const app = document.querySelector('#app')
 
 function render() {
   // 여기에 코딩해주세요!
+  for(let i=0;i<todoList.length;i++){
+    const div = document.createElement("div");
+    app.appendChild(div);
+    div.innerText = todoList[i].title
+  }
 }
 
 render()
