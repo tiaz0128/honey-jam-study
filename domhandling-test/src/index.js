446880d291f4aaa743dab3240e74c904a611c62d
bom <kimu_u@naver.com>
2021-04-20 23:37:33 +0900

    "토글 등록"

--- domhandling-test/src/index.js
+++ domhandling-test/src/index.js
@@ -1,1 +1,12 @@
 // 코드를 작성해주세요!
+const app = document.querySelector('#app');
+const toggleBt = document.querySelector('#toggleAll-btn');
+const tree = document.querySelector('#tree');
+const fishes = tree.childNodes[3];
+const animalCh = tree.querySelector('ul');
+const fishesCh = fishes.querySelector('ul');
+
+toggleBt.addEventListener('click',() => {
+    animalCh.classList.toggle('toggle');
+    fishesCh.classList.toggle('toggle');
+})
